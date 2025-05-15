b67ede16cad8b1b1c095c00da1d74d10fa64a80c
petrovska-nika <petrovska.nika.dev@gmail.com>
2025-05-15 15:14:41 +0300

    hw11

---
+++ src/main.js
@@ -0,0 +1,52 @@
+import './css/styles.css';
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+import { getImagesByQuery } from './js/pixabay-api.js';
+import {
+  createGallery,
+  clearGallery,
+  showLoader,
+  hideLoader,
+} from './js/render-functions.js';
+
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', async event => {
+  event.preventDefault();
+  const query = event.target.elements['search-text'].value.trim();
+
+  if (!query) {
+    iziToast.error({
+      title: 'Error',
+      message: 'Search field cannot be empty!',
+    });
+    return;
+  }
+
+  showLoader();
+  clearGallery();
+
+  try {
+    const data = await getImagesByQuery(query);
+
+    if (data.hits.length === 0) {
+      iziToast.warning({
+        title: 'No Results',
+        message:
+          'Sorry, there are no images matching your search query. Please try again!',
+      });
+      return;
+    }
+
+    createGallery(data.hits);
+  } catch (error) {
+    iziToast.error({
+      title: 'Error',
+      message: 'Something went wrong. Try again later!',
+    });
+    console.error(error);
+  } finally {
+    hideLoader();
+  }
+});
